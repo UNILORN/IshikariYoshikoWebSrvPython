762335d12418386da21eac358960ee3321863099
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-12-11 03:57:23 +0900

    DB挿入完了

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 import time
 from os.path import join, dirname
 from dotenv import load_dotenv
+import json
+import MySQLdb
 
 def WebsocketConnect():
     dotenv_path = join(dirname(__file__), '.env')
@@ -20,14 +22,69 @@ def WebsocketConnect():
     ws = create_connection(ws_url)
     return ws
 
-def WebsocketReceive(ws):
+def WebsocketReceive(con,ws):
      while True:
         result =  ws.recv()
-        print(result)
+        db_insert(con,json.loads(result))
+
+
+ 
+def db_connect():
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    # 接続する 
+    con = MySQLdb.connect(
+            user=os.environ.get("DB_USERNAME"),
+            passwd=os.environ.get("DB_PASSWORD"),
+            host='localhost',
+            db=os.environ.get("DB_DATABASE"))
+ 
+    return con
+
+def db_insert(con,data):
+    
+    # カーソルを取得する
+    cur= con.cursor()
+     
+    # クエリを実行する
+    tableList = {
+        "pressure":data["pressure"],
+        "accelerometer_z":data["accelerometer_z"],
+        "gyroscope_y":data["gyroscope_y"],
+        "gyroscope_x":data["gyroscope_x"],
+        "gyroscope_z":data["gyroscope_z"],
+        "degrees_y":data["degrees_y"],
+        "temperature":data["temperature"],
+        "degrees_r":data["degrees_r"],
+        "degrees_p":data["degrees_p"],
+        "accelerometer_y":data["accelerometer_y"],
+        "accelerometer_x":data["accelerometer_x"],
+        "humidity":data["humidity"],
+        "compass_x":data["compass_x"],
+        "compass_y":data["compass_y"],
+        "compass_z":data["compass_z"]
+    }
+    column = ""
+    values = ""
+    for k,v in tableList.items():
+        column = column + k + ","
+        values = values + str(v) + ","
+    
+    column = column[:-1]
+    values = values[:-1]
+
+    sql = "insert into sensor ("+column+") values("+values+");"
+    cur.execute(sql)
+    con.commit()
+
+    print("[ Database Insert ] SUCCESS!!!")
+    cur.close
 
 if __name__ == '__main__':
     ws = WebsocketConnect()
-    WebsocketReceive(ws)
-
+    con = db_connect()
+    WebsocketReceive(con,ws)
+    con.close
     ws.close()
 
